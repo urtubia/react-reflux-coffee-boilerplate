e30c97efd1d426bb476711e52f9096796576e6a9
Hector Urtubia <urtubia@gmail.com>
2015-10-03 11:43:47 -0700

    Fixing production task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var stylus = require('gulp-stylus');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
+
+function build_css(src, outfile){
+  gulp.src(src)
+  .pipe(stylus())
+  .pipe(gulp.dest('public/'));
+}
 
 function watch_css(watch,src){
   gulp.watch(watch,function(){
@@ -36,16 +43,18 @@ function watch_bundler(src, outfile){
 }
 
 function build_browserify(src, outfile){
-  var bundler = watchify(browserify(src, {
+  var bundler = browserify({
+    entries: src,
     cache: {},
     packageCache: {},
     fullPaths: false,
     debug: false,
-  }));
+  });
   bundler.transform('coffee-reactify', { extension: ".cjsx"});
   return bundler.bundle()
-  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
   .pipe(source(outfile))
+  .pipe(buffer())
+  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
   .pipe(streamify(uglify()))
   .pipe(gulp.dest('./public'));
 }
@@ -57,4 +66,5 @@ gulp.task('default', function(){
 
 gulp.task('production', function(){
   build_browserify('./src/main.coffee', 'bundle.js');
+  build_css(['./src/style/*.styl'],'src/style/main.styl');
 });
